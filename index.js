84fda4a57f08d42b359420eeaa57b6df47aeec8a
Rafael Louie Miguel <118577897+egg-lou@users.noreply.github.com>
2024-02-20 14:21:52 +0800

    test

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello, World!" });
+});
+
+app.get("/get_requests", (req, res) => {
+    fs.readFile("data.json", "utf-8", (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error reading data file");
+            return;
+        }
+
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post("/create_request", (req,res) => {
+    const requestData = req.body;
+
+    fs.writeFile("newData.json", JSON.stringify(requestData), (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error writing data file");
+            return;
+        }
+
+        res.json({ message: "Request created successfully" });
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
